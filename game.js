303367f35fd3b85367370080580f8ca52ce17c12
Robert B <robertb@lifesciencepublishers.com>
2020-11-25 12:53:48 -0700

    Reading Game

--- game.js
+++ game.js
@@ -6,61 +6,72 @@ const card = document.getElementsByClassName('card');
 const message = document.getElementById('message');
 const filePath = './word_list.json';
 
-let voices = [];
 let utterThis = new SpeechSynthesisUtterance();
+let wordToFind = "";
 
-function getVoices() {
-    voices = synth.getVoices();
-    voices.forEach(voice => {
-      const option = document.createElement('option');
-      if(voice.name ==="Microsoft Zira Desktop - English (United States)"){
-        option.selected = true;
-      }
-      option.value = voice.name;
-      option.innerText = `${voice.name} ${voice.lang}`;
-      voicesSelect.appendChild(option);
-    });
-  }
-  
-  function setVoice(e){
-      console.log(e.target.value);
-      const test = voices.find(voice => voice.name === e.target.value);
-      utterThis.voice = test;
+async function getWordList(level = 0){
+  const res = await fetch(filePath);
+  const data = await res.json();
+  const wordList = data[level];
+  return wordList.wordList
+ }
+
+ function createWordCard() {
+  getWordList(1).then(wordList =>{
+    wordList.forEach(word =>{
+      const card = `
+      <div class="card" onclick="cardClick(this)">
+        <h3>${word}</h3>
+      </div>`;
+      cardList.innerHTML += card;
+     })
+  });
  }
 
  function cardClick(element) {
     const elementWord = element.textContent.trim().toLowerCase();
-    if(elementWord === wordToFind.toLowerCase()){
-      message.innerHTML = "<h3>Correct</h3>";
+    if(wordToFind === ""){
+      getWord();
     } else {
-      message.innerHTML = "<h3>Wrong</h3>";
+      if(elementWord === wordToFind.toLowerCase()){
+        const text = 'Correct!!';
+        message.innerHTML = `<h3>${text}</h3>`;
+        utterThis.text = text;
+        synth.speak(utterThis);
+        setTimeout(()=>{
+          message.innerHTML = "";
+          getWord()
+        },2000);
+      } else {
+        const text = `No, that word is ${elementWord}.`;
+        message.innerHTML = `<h3>${text}</h3>`;
+        utterThis.text = text;
+        synth.speak(utterThis);
+        setTimeout(function(){ 
+          utterThis.text = `Find the word...${wordToFind}`;
+          synth.speak(utterThis);
+          message.innerHTML = "";
+        }, 2000);
+      }
     }
  }
 
- async function createWordCard() {
-   const res = await fetch(filePath);
-   const data = await res.json();
-   const wordList = data.level_01.word_list;
-   wordList.forEach(word =>{
-    const card = `
-    <div class="card" onclick="cardClick(this)">
-      <h3>${word}</h3>
-    </div>`;
-    cardList.innerHTML += card;
-   })
- }
- createWordCard();
-
- async function getWord(){
-  const res = await fetch(filePath);
-  const data = await res.json();
-  const wordList = data.level_01.word_list;
-  utterThis.text = `Find the word... ${wordList[1]}`;
-  synth.speak(utterThis);
-  console.log(wordList[1])
+ function getWord(){
+  getWordList(1).then(wordList => {
+    const randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+    sayWordToFind(randomWord);
+    setWordToFind(randomWord);
+  })
  }
 
-synth.addEventListener('voiceschanged', getVoices);
-voicesSelect.addEventListener('change', setVoice);
+ function setWordToFind(randomWord) {
+    wordToFind = randomWord;
+ }
+
+ function sayWordToFind(randomWord) {
+    utterThis.text = `Find the word...${randomWord}`;
+    synth.speak(utterThis);
+ }
 
 readText.addEventListener('click', getWord);
+createWordCard();
